282ac6e636198b8d4787bb52ee9b86c6566ab782
Ilya Senatorov <il.senatorov@protonmail.com>
2023-11-09 16:55:18 +0100

    simple diffusion

--- diffuse.py
+++ diffuse.py
@@ -9,13 +9,11 @@ from src.data.datasets import KilterDiffusionDataset
 torch.set_float32_matmul_precision("medium")
 
 parser = argparse.ArgumentParser(description="Your program description")
-parser.add_argument(
-    "--embedding_dim", type=int, default=256, help="Size of embedding for image"
-)
 parser.add_argument("--dim", type=int, default=64, help="Model dimension")
 parser.add_argument("--timesteps", type=int, default=1000, help="Timesteps")
 parser.add_argument("--batch_size", type=int, default=64, help="Batch size")
 parser.add_argument("--num_workers", type=int, default=16, help="Number of workers")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
 args = parser.parse_args()
 
 
@@ -34,7 +32,7 @@ wandb.init(config=config, project="kilter_diffusion")
 trainer = pl.Trainer(
     accelerator="gpu",
     devices=-1,
-    logger=pl.loggers.WandbLogger(log_model=True, save_dir="logs"),
+    logger=pl.loggers.WandbLogger(log_model="all", save_dir="logs"),
     max_epochs=1000,
     precision="bf16-mixed",
 )
--- src/models/diffusion_unet.py
+++ src/models/diffusion_unet.py
@@ -1,5 +1,5 @@
 import torch
-from denoising_diffusion_pytorch import Unet, GaussianDiffusion
+from denoising_diffusion_pytorch.simple_diffusion import UViT, GaussianDiffusion
 import pytorch_lightning as pl
 from ..utils.vis import plot_climb
 
@@ -7,12 +7,9 @@ from ..utils.vis import plot_climb
 class DiffusionUNet(pl.LightningModule):
     def __init__(self, config):
         super().__init__()
-        unet = Unet(
-            dim=config["dim"], dim_mults=(1, 2, 4, 8), flash_attn=True, channels=4
-        )
-        self.diffusion = GaussianDiffusion(
-            unet, image_size=48, timesteps=config["timesteps"]
-        )
+        unet = UViT(dim=config["dim"], dim_mults=(1, 2, 4, 8), channels=4)
+        self.diffusion = GaussianDiffusion(unet, image_size=48, channels=4)
+        self.lr = config["lr"]
 
     def forward(self, img):
         loss = self.diffusion(img.float())
@@ -21,15 +18,15 @@ class DiffusionUNet(pl.LightningModule):
     def training_step(self, batch):
         x, angle, difficulty = batch
         loss = self.forward(x)
-        if self.global_step % 100 == 1:
+        if self.global_step % 500 == 1:
             sampled_images = self.diffusion.sample(batch_size=4)
             self.logger.log_image(
                 key="samples",
-                images=[plot_climb((x > 0.7).long()) for x in list(sampled_images)],
+                images=[plot_climb((x > 0.5).long()) for x in list(sampled_images)],
             )
         self.log("train/loss", loss)
         return loss
 
     def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=1e-4)
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr)
         return optimizer
